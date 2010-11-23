92c538b5c53f786c8b6df429d3d3449fa56aa17a
Doug <dougdance@gmail.com>
2010-11-23 10:31:21 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stephanie::Application.config.secret_token = '3d5c405d6852b13b34e9d26922609c75492dbd059abf2a2a2ad1d3adb65d1252dd0987bed2c7b06cca8afbb58de823f7583c58d4e40fdf60932b106d4c280c0f'
